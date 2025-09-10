237857e00c94f864aac9890041f321531c12d691
libiaf <A01253425@tec.mx>
2025-09-10 10:03:13 -0700

    Definir interfaces

--- src/apis/apihotel.ts
+++ src/apis/apihotel.ts
@@ -7,3 +7,17 @@ const headers = {
   "x-rapidapi-key": RAPID_API_KEY,
   "x-rapidapi-host": RAPID_API_HOST,
 };
+
+export interface FrancisInput {
+  city: string;
+  start_date: string;
+  end_date: string;
+  category: string[];
+  price: number;
+  hotel: string;
+}
+
+export interface HotelFacilitiesOutput {
+  hotel_name: string;
+  facilities: string[];
+}
